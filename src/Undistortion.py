c5a920e0f3cc972d7ee3f687430598836f101e7e
denis-chusovitin <chusovitin.den@gmail.com>
2015-11-25 14:18:18 +0300

    Add undistortion

---
+++ src/Undistortion.py
@@ -0,0 +1,11 @@
+import cv2
+
+def undistort(img, mtx, dist):
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    x, y, w, h = roi
+    dst = dst[y:y + h, x:x + w]
+    return dst
